b3782ddb963b9d4a1e872f314671de18b92f70dd
Chris Allen <callen7908@yahoo.com>
2021-05-01 18:38:05 -0400

    Embedded a youtube video

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <div>
             <p>You are on the Home page</p>
+            <iframe width="420" height="315"src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1"> </iframe>
         </div>
     );
 }
