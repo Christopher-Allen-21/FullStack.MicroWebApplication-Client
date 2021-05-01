aaebc7e79d3ed94820ddd906ed2c942532143c24
Chris Allen <callen7908@yahoo.com>
2021-05-01 18:55:33 -0400

    Added multiple embedded videos

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,7 +5,23 @@ const Home = () => {
     return (
         <div>
             <p>You are on the Home page</p>
-            <iframe width="420" height="315"src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1"> </iframe>
+
+
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/9vwFciHiWlk"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/9vwFciHiWlk"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/9vwFciHiWlk"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
         </div>
     );
 }
