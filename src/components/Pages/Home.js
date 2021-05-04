82dbaf656dd6be3af372c9c5bd35bbd4db75054e
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-05-04 13:20:41 -0400

    made changes to the home.js file

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -12,12 +12,12 @@ const Home = () => {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen></iframe>
 
-            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/9vwFciHiWlk"
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/4p_bqtmrw-E"
                     title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen></iframe>
 
-            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/9vwFciHiWlk"
+            <iframe className ="embedded-video"   width="420" height="315" src="https://www.youtube.com/embed/jSRNaO_41Y0"
                     title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen></iframe>
