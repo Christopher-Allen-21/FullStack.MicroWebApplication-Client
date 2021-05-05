c33534005977dd460a40cbfc662a8cabdfbaa8c7
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-05-05 17:06:36 -0400

    added videos to the home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -22,11 +22,55 @@ const Home = () => {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen></iframe>
 
+            <iframe className="embedded-video"   width="420" height="315" src="https://www.youtube.com/embed/4n6LrehCPOQ"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/oeqPrUmVz-o"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/mFSe1EhnZkw"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/e50wvKQvDHQ"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video"  width="420" height="315" src="https://www.youtube.com/embed/hOHvMqAgcmc"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/L3oOldViIgY"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/Zv8-hrGiGno"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/OVvTv9Hy91Q"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
+            <iframe className="embedded-video" width="420" height="315" src="https://www.youtube.com/embed/Qf-wVa9y9V4"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+
         </>
 
 
 
     );
 }
-
 export default Home;
