dc4f3d8b6269dc58ee392d713569b1bb9c96ce5f
Chris Allen <callen7908@yahoo.com>
2021-05-05 09:19:41 -0400

    Added carousel to explore

--- src/components/Pages/Explore.js
+++ src/components/Pages/Explore.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import '../../styling/Pages/Explore.css'
+import React, { useEffect, useState } from "react";
+import '../../styling/Pages/Explore.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExploreCarousel from "../PartsOfPage/ExploreCarousel";
+import Carousel from 'react-bootstrap/Carousel';
+
 
 const Explore = () => {
     const url = "http://localhost:8090/file/download/2_Gracie.mp4"
+
 
     return (
         <>
@@ -19,21 +24,63 @@ const Explore = () => {
             <hr />
 
             <p><strong>You might also be interested in...</strong></p>
-            <video className="explore-video" controls muted>
-                <source src={url} type="video/mp4"></source>
-            </video>
-            <video className="explore-video" controls muted>
-                <source src={url} type="video/mp4"></source>
-            </video>
-            <video className="explore-video" controls muted>
-                <source src={url} type="video/mp4"></source>
-            </video>
-            <video className="explore-video" controls muted>
-                <source src={url} type="video/mp4"></source>
-            </video>
-            <video className="explore-video" controls muted>
-                <source src={url} type="video/mp4"></source>
-            </video>
+
+            <ExploreCarousel show={3}>
+                <video className="explore-video-carousel" style={{padding: 8}} controls muted>
+                    <source src={url} type="video/mp4"></source>
+                </video>
+                <video className="explore-video-carousel" style={{padding: 8}} controls muted>
+                    <source src={url} type="video/mp4"></source>
+                </video>
+                <video className="explore-video-carousel" style={{padding: 8}} controls muted>
+                    <source src={url} type="video/mp4"></source>
+                </video>
+                <video className="explore-video-carousel" style={{padding: 8}} controls muted>
+                    <source src={url} type="video/mp4"></source>
+                </video>
+                <video className="explore-video-carousel" style={{padding: 8}} controls muted>
+                    <source src={url} type="video/mp4"></source>
+                </video>
+            </ExploreCarousel>
+
+
+
+
+            {/*<Carousel id="explore-carousel">*/}
+            {/*    <Carousel.Item>*/}
+            {/*        <video className="explore-video-carousel" controls muted>*/}
+            {/*            <source src={url} type="video/mp4"></source>*/}
+            {/*        </video>*/}
+            {/*    </Carousel.Item>*/}
+            {/*    <Carousel.Item>*/}
+            {/*        <video className="explore-video-carousel" controls muted>*/}
+            {/*            <source src={url} type="video/mp4"></source>*/}
+            {/*        </video>*/}
+            {/*    </Carousel.Item>*/}
+            {/*    <Carousel.Item>*/}
+            {/*        <video className="explore-video-carousel" controls muted>*/}
+            {/*            <source src={url} type="video/mp4"></source>*/}
+            {/*        </video>*/}
+            {/*    </Carousel.Item>*/}
+            {/*    <Carousel.Item>*/}
+            {/*        <video className="explore-video-carousel" controls muted>*/}
+            {/*            <source src={url} type="video/mp4"></source>*/}
+            {/*        </video>*/}
+            {/*    </Carousel.Item>*/}
+            {/*</Carousel>*/}
+
+            {/*<video className="explore-video" controls muted>*/}
+            {/*    <source src={url} type="video/mp4"></source>*/}
+            {/*</video>*/}
+            {/*<video className="explore-video" controls muted>*/}
+            {/*    <source src={url} type="video/mp4"></source>*/}
+            {/*</video>*/}
+            {/*<video className="explore-video" controls muted>*/}
+            {/*    <source src={url} type="video/mp4"></source>*/}
+            {/*</video>*/}
+            {/*<video className="explore-video" controls muted>*/}
+            {/*    <source src={url} type="video/mp4"></source>*/}
+            {/*</video>*/}
 
         </>
     );
--- src/components/PartsOfPage/SubSidebar.js
+++ src/components/PartsOfPage/SubSidebar.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import '../../styling/PartsOfPage/SubSidebar.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
 const SubSidebar = () => {
     return (
@@ -16,19 +13,6 @@ const SubSidebar = () => {
             <a href="/subscriptions" className="side-bar-link">Subscription 5</a>
             <a href="/subscriptions" className="side-bar-link">Subscription 6</a>
         </div>
-
-        // <>
-        //     <Nav className="side-bar-container" defaultActiveKey="/home" className="flex-column">
-        //         <p id="subscription-title"><strong>Subscriptions</strong></p>
-        //         <Nav.Link href="/home">Subscription 1</Nav.Link>
-        //         <Nav.Link href="/home">Subscription 2</Nav.Link>
-        //         <Nav.Link href="/home">Subscription 3</Nav.Link>
-        //         <Nav.Link href="/home">Subscription 4</Nav.Link>
-        //         <Nav.Link href="/home">Subscription 5</Nav.Link>
-        //         <Nav.Link href="/home">Subscription 6</Nav.Link>
-        //     </Nav>
-        //
-        // </>
     );
 }
 
