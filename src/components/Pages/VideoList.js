3933d54182fc881d008b50c2eca55cb33363740c
Chris Allen <callen7908@yahoo.com>
2021-05-07 10:01:24 -0400

    Able to pull .mp4 from S3 and json data from RDS and present on screen

--- src/components/Pages/VideoList.js
+++ src/components/Pages/VideoList.js
@@ -1,26 +1,61 @@
 import React from "react";
 import '../../styling/Pages/VideoList.css';
 
-const VideoList = () => {
-    const url = "http://localhost:8090/file/download/";
-    const urlWithId = [];
-    for(var i=0; i<81; i++){
-        var urlPlusId = url+i;
-        urlWithId.push(urlPlusId)
+class VideoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
     }
 
-    const allVideos = urlWithId.map((urlWithId) =>
-        <video className="videolist-video" src={urlWithId} type="video/mp4" controls muted></video>
-    );
+    componentDidMount() {
+        fetch("http://localhost:8090/video")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
+    render() {
+        const { error, isLoaded, items } = this.state;
 
-    return (
-        <ul>
-            {allVideos}
-        </ul>
-    )
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        }
+        else if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+        else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.videoId}>
+                            Video ID:{item.videoId}  Title:{item.title}  User ID:{item.userId}  Length:{item.lengthOfVideo}  View Count:{item.viewCount}  Description:{item.description}  Like Count:{item.likeCount} Dislike Count:{item.dislikeCount}  Category:{item.category}
+
+                            <video className="videolist-video" src={`http://localhost:8090/file/download/${item.videoId}`} type="video/mp4" controls muted></video>
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
 }
-
-
 
 export default VideoList;
