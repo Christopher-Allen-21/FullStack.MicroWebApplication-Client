c71062cdc0e15059208c809e969672c6f1899dfb
theresa-mashura <theresamashura@gmail.com>
2021-05-09 00:22:11 -0400

    like and dislike fully working and formatted

--- src/components/Pages/PlayVideo.js
+++ src/components/Pages/PlayVideo.js
@@ -1,5 +1,10 @@
 import React from "react";
 import '../../styling/Pages/PlayVideo.css';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import {ToggleButtonGroup} from "react-bootstrap";
+import Card from 'react-bootstrap/Card'
 
 class PlayVideo extends React.Component {
     constructor(props) {
@@ -8,6 +13,12 @@ class PlayVideo extends React.Component {
             error: null,
             isLoaded: false,
             video: null,
+
+            // Like & Dislike Buttons
+            likeClicks: 0,
+            dislikeClicks: 0,
+            likeDisabled: false,
+            dislikeDisable: false
         };
     }
 
@@ -36,9 +47,34 @@ class PlayVideo extends React.Component {
             )
     }
 
-    incrementLikes = () => {
+    clickLikeButton = () => {
+        // FIRST CLICK OF LIKE BUTTON
+        if (this.state.likeClicks === 0) {
+            this.changeLikes("plus")
+            this.setState( {likeClicks: 1, dislikeDisabled: true, likeDisabled: false} ); // CANNOT DISLIKE IF LIKED
+        // SECOND CLICK OF LIKE BUTTON
+        } else {
+            this.changeLikes("minus");
+            this.setState( {likeClicks: 0, dislikeDisabled: false, likeDisabled: false} ); // IF UNLIKED, REVERT BACK TO ORIGINAL STATE
+        }
+    }
+
+    clickDislikeButton = () => {
+        // FIRST CLICK OF DISLIKE BUTTON
+        if (this.state.dislikeClicks === 0) {
+            this.changeDislikes("plus")
+            this.setState( {dislikeClicks: 1, dislikeDisabled: false, likeDisabled: true} ); // CANNOT LIKE IF DISLIKED
+        // SECOND CLICK OF DISLIKE BUTTON
+        } else {
+            this.changeDislikes("minus");
+            this.setState( {dislikeClicks: 0, dislikeDisabled: false, likeDisabled: false} ); // IF UN-DISLIKED, REVERT BACK TO ORIGINAL STATE
+        }
+    }
+
+    // INCREMENT/DECREMENT LIKE COUNT BY 1 ON SERVER
+    changeLikes = (plusOrMinus) => {
         const { videoId } = this.props.location.state;
-        fetch(`http://localhost:8090/video/like/${videoId}`,
+        fetch(`http://localhost:8090/video/like/${plusOrMinus}/${videoId}`,
             {method: "PATCH"})
             .then( response => response.json() )    // parse body test as JSON
             .then( result => {
@@ -48,9 +84,10 @@ class PlayVideo extends React.Component {
             })
     }
 
-    decrementLikes = () => {
+    // INCREMENT/DECREMENT DISLIKE COUNT BY 1 ON SERVER
+    changeDislikes = (plusOrMinus) => {
         const { videoId } = this.props.location.state;
-        fetch(`http://localhost:8090/video/dislike/${videoId}`,
+        fetch(`http://localhost:8090/video/dislike/${plusOrMinus}/${videoId}`,
             {method: "PATCH"})
             .then( response => response.json() )    // parse body test as JSON
             .then( result => {
@@ -72,15 +109,30 @@ class PlayVideo extends React.Component {
         else {
             return (
                 <div className="playVideo-container">
+
+                    {/* VIDEO */}
                     <video className="playVideo-video" src={`http://localhost:8090/file/download/${this.state.video.videoId}`} type="video/mp4" controls muted></video>
                     <br />
+
+                    {/* VIDEO TITLE */}
                     <h1 id="playVideo-title">{this.state.video.title}</h1>
                     <p className="playVideo-paragraph">
-                        <button onClick={this.incrementLikes}>Like</button> {this.state.video.likeCount} Likes &nbsp;
-                        <button onClick={this.decrementLikes}>Dislike</button> {this.state.video.dislikeCount} Dislikes<br />
+                        {/* LIKE AND DISLIKE BUTTONS */}
+                        <ButtonGroup className="like-dislike-buttons" type="checkbox" defaultValue={[1]} aria-label="like-dislike-buttons">
+                            <Button variant="success" value={0} disabled={this.state.likeDisabled} onClick={this.clickLikeButton}>Like</Button>
+                            <Button variant="danger" value={0} disabled={this.state.dislikeDisabled} onClick={this.clickDislikeButton}>Dislike</Button>
+                        </ButtonGroup>
+                        &nbsp; {this.state.video.likeCount} Likes
+                        &nbsp; {this.state.video.dislikeCount} Dislikes <br/>
+
+                        {/* PAGE VIEWS AND POSTED DATE */}
                         {this.state.video.viewCount} views - Posted on {this.state.video.videoPostedDate.substr(0,10)}<br />
+
+                        {/* VIDEO DESCRIPTION */}
                         <strong>Description:</strong><br />
-                        <span>{this.state.video.description}</span><br />
+                        <Card body>{this.state.video.description}</Card><br />
+
+                        {/* VIDEO CATEGORY */}
                         <strong>Category: </strong>{this.state.video.category}
                     </p>
                 </div>
