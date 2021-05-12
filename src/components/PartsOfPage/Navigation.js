e8916787ca491acdfc117c97a922341b1047f02b
theresa-mashura <theresamashura@gmail.com>
2021-05-12 01:38:17 -0400

    added auth0 login

--- src/components/PartsOfPage/Navigation.js
+++ src/components/PartsOfPage/Navigation.js
@@ -6,8 +6,13 @@ import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import AuthNav from "../../auth/AuthNav.js"
+import {useAuth0} from "@auth0/auth0-react";
+import Loading from "../Authentication/Loading.js"
+
 
 const Navigation = () => {
+
     return (
         <>
             <Navbar className="nav-bar-container" bg="dark" variant="dark" fixed="top">
@@ -18,6 +23,8 @@ const Navigation = () => {
                     <Nav.Link href="/subscriptions">Subscriptions</Nav.Link>
                     <Nav.Link href="/history">History</Nav.Link>
                     <Nav.Link href="/support">Support</Nav.Link>
+                    <Nav.Link href="/profile">Profile</Nav.Link>
+                    <AuthNav/>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+      <Router>
+        <Auth0ProviderWithHistory>
+            <App />
+        </Auth0ProviderWithHistory>
+      </Router>
+,
   document.getElementById('root')
 );
 
