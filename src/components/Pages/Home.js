8f50c0d6d30ec35785e3d04dff59a481bab407d1
Chris Allen <callen7908@yahoo.com>
2021-05-12 14:44:34 -0400

    Replaced subscriptions on sidebar with categories. Also removed subscriptions from nav bar

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -22,9 +22,6 @@ class Home extends React.Component {
                                         items: result
                                 });
                         },
-                        // Note: it's important to handle errors here
-                        // instead of a catch() block so that we don't swallow
-                        // exceptions from actual bugs in components.
                         (error) => {
                                 this.setState({
                                         isLoaded: true,
--- src/components/Pages/VideosByCategory.js
+++ src/components/Pages/VideosByCategory.js
@@ -2,9 +2,6 @@ import React from "react";
 import '../../styling/Pages/VideosByCategory.css';
 import {Link} from "react-router-dom";
 
-//import VideoThumbnail from 'react-video-thumbnail'; // use npm published version
-
-
 class VideosByCategory extends React.Component {
     constructor(props) {
         super(props);
@@ -26,9 +23,6 @@ class VideosByCategory extends React.Component {
                         items: result
                     });
                 },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
                 (error) => {
                     this.setState({
                         isLoaded: true,
@@ -55,14 +49,6 @@ class VideosByCategory extends React.Component {
                             <strong>Video ID:</strong> {video.videoId}<br />
                             <Link to={{pathname: '/play', state: {videoId: video.videoId}}}>
                                 <video controlsList="nofullscreen nodownload" className="video-list-video" src={`http://localhost:8090/file/download/${video.videoId}`} type="video/mp4" controls muted></video>
-                                {/*Creates a thumbnail of the video at the 1 second mark, but it messes up the formatting on the rest of the page*/}
-                                {/*<VideoThumbnail className="video-list-video"*/}
-                                {/*    videoUrl={`http://localhost:8090/file/download/${video.videoId}`}*/}
-                                {/*    thumbnailHandler={(thumbnail) => console.log(thumbnail)}*/}
-                                {/*    snapshotAtTime={1}*/}
-                                {/*    width={240}*/}
-                                {/*    height={160}*/}
-                                {/*/>*/}
                             </Link>
                             <br />
                             <h1 id="video-list-title">{video.title}</h1>
